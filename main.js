04e13f4a234a288817b6a4a04d62daf6d1ddeecd
shibaaa <k.shibata9447@gmail.com>
2020-08-31 17:19:50 +0900

    Initial commit

---
+++ main.js
@@ -0,0 +1,32 @@
+const app = new Vue({
+  el: '#app',
+  data: {
+    newTodo: '',
+    todos: []
+  },
+  mounted () {
+    if (localStorage.getItem('todos')) {
+      try {
+        this.todos = JSON.parse(localStorage.getItem('todos'))
+      } catch (err) {
+        localStorage.removeItem('todos')
+      }
+    }
+  },
+  methods: {
+    addTodo () {
+      if (!this.newTodo) return
+      this.todos.push(this.newTodo)
+      this.newTodo = ''
+      this.saveTodo()
+    },
+    removeTodo (index) {
+      this.todos.splice(index, 1)
+      this.saveTodo()
+    },
+    saveTodo () {
+      const parsed = JSON.stringify(this.todos)
+      localStorage.setItem('todos', parsed)
+    }
+  }
+})
